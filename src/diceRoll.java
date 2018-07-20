403d7f840481fbb6702f4903901fbf6b014e8f50
Matthew <mrhadigan@gmail.com>
2018-07-19 20:22:43 -0400

    second commit

--- src/diceRoll.java
+++ src/diceRoll.java
@@ -5,13 +5,14 @@ public class diceRoll {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		// Random rand = new Random();
-		
+	
+		// Asking for user input, and scanning in user input
 		System.out.println("Please enter the number of sides on the dice you are about to throw: ");
 		int diceSides = scan.nextInt();
 		scan.nextLine();
 		
 		String cont;
+		// a do loop to roll the dice and get the random number from the method
 		do {
 		System.out.println("Rolling Dice!");
 		int num1 = rollDice(diceSides);
@@ -19,6 +20,7 @@ public class diceRoll {
 		System.out.println("" + num1 + " " + num2);
 		special(num1, num2);
 		
+		// asking if user wants to continue, and stoping in they say anything other than "y"
 		System.out.println("Continue? (y/n)");
 		cont = scan.nextLine();
 		
@@ -27,6 +29,7 @@ public class diceRoll {
 		
 	}
 
+	// A method to get the random number of each dice
 	private static int rollDice(int diceSides) {
 		Random rand = new Random();
 		int randomSide = rand.nextInt(diceSides);
@@ -34,6 +37,7 @@ public class diceRoll {
 	  
 	}
 	
+	// A method to get specific outputs for specific rolls of the dice
 	private static void special(int num1, int num2) {
 		if (num1 == 1 && num2 == 1) {
 			System.out.println("Snake Eyes");
